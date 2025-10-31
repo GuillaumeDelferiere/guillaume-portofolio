eab254752fc05536f08c74446cf0eea9216c130d
GuillaumeDelferiere <guigui.delferiere@gmail.com>
2025-10-31 17:05:54 +0100

    Correction pour le déploiement

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  reactCompiler: true,
+  experimental: {
+    reactCompiler: true,
+  },
 };
 
 export default nextConfig;
