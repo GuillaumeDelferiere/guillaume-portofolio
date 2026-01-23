ca13cd4e9974a98a64ba375c953c8f36ec4dc6a1
GuillaumeDelferiere <guigui.delferiere@gmail.com>
2026-01-23 19:22:33 +0100

    petite correction

--- src/app/(components)/hero.tsx
+++ src/app/(components)/hero.tsx
@@ -73,7 +73,7 @@ export default function Hero() {
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.6 }}
       >
-        Guillaume Delferiere — Développeur junior full-stack
+        Guillaume Delferiere - Développeur junior full-stack
       </motion.h1>
 
       <motion.p
